c934c5f9120d04da61fc24896137bebbb14c920c
Abdelrahman Adel <Abdelrahman7adel@gmail.com>
2025-01-12 14:26:23 +0200

    Regester DbContext

--- DoctorBooking/Program.cs
+++ DoctorBooking/Program.cs
@@ -1,12 +1,16 @@
 using DoctorAvailability.API;
+using DoctorBooking.Shared;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Register ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers().AddApplicationPart(typeof(DoctorController).Assembly);
 var app = builder.Build();
@@ -18,9 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
